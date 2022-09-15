a2bbba6c2ea0a37dec03dacf623d09a51ecfda58
tek <tek@randhome.io>
2022-09-15 18:16:25 +0200

    Bumps version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='sdanalyzer',
-    version='0.1.6',
+    version='0.1.7',
     description='Tool to analyze APK files',
     long_description=long_description,
     long_description_content_type="text/markdown",
